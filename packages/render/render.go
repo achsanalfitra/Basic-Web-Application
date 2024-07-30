fef76ffe0855741c8bbafd50f9c4e55792deb2e5
achsanalfitra <achsanalfitra@gmail.com>
2024-07-30 23:12:24 +0700

    add template cache function

--- packages/render/render.go
+++ packages/render/render.go
@@ -6,10 +6,27 @@ import (
 	"net/http"
 )
 
+var templateCache = make(map[string]*template.Template)
+
 func TemplateParser(w http.ResponseWriter, html string) {
-	parsedTemplate, _ := template.ParseFiles("./html-templates/" + html)
-	err := parsedTemplate.Execute(w, nil)
+	_, cacheContent := templateCache[html]
+
+	if !cacheContent {
+		fmt.Println("Creating new template for", html)
+		templateCacher(html)
+	} else {
+		fmt.Println("Using existing template for", html)
+	}
+
+	cachedTemplate := templateCache[html]
+
+	err := cachedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("Error parsing the HTML template")
 	}
+}
+
+func templateCacher(html string) {
+	createdTemplate, _ := template.ParseFiles("./html-templates/" + html)
+	templateCache[html] = createdTemplate
 }
